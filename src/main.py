3a9aa6783eb33c79ffebf455672f97d658d2db6b
Spiky <ryan.j@epoxstudios.com>
2022-01-18 18:08:45 -0500

    Changed testing

--- src/main.py
+++ src/main.py
@@ -4,6 +4,8 @@ import threading
 e = Elevator(10)
 e.PushButton(True, 8)
 e.PushButton(True, 2)
-e.PushButton(True, 6)
-e.PushButton(True, 4)
+e.PushButton(False, 6)
+e.PushButton(False, 4)
 e.PushButton(True, 10)
+# should be 2,8,10,6,4
+# ended up being 10, 10, 4, 8, 6, 2, 10
