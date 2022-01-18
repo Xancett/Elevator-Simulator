a5fc76df4338f428eefc7ea5cb1d5f93e39952b9
Spiky <ryan.j@epoxstudios.com>
2022-01-18 17:12:16 -0500

    Started test with elevator

--- src/main.py
+++ src/main.py
@@ -1,4 +1,9 @@
 from elevator import Elevator
+import threading
 
 e = Elevator(10)
-print(e.getLocation())
+e.PushButton(True, 8)
+e.PushButton(True, 2)
+e.PushButton(True, 6)
+e.PushButton(True, 4)
+e.PushButton(True, 10)
