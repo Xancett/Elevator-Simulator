5958a1609c8011f764cc58bae906496fe9e1c88b
Spiky <ryan.j@epoxstudios.com>
2022-01-18 17:11:57 -0500

    Created event handlers as well as updated function for moving the elevator

--- src/elevator.py
+++ src/elevator.py
@@ -1,6 +1,20 @@
+import threading
 class Elevator:
 	def __init__(self, floors):
 		self.floors = floors
 		self.location = 0
+		self.queue = [floors]
+		self.UpdatePosition()
 	def getLocation(self):
 		return self.location
+	def PushButton(self, up, floor):
+		print("Pushed button")
+		print("Request on floor ", floor)
+		self.queue.append(floor)
+	def ChooseFloor(self, floor):
+		print("Picked a floor")
+	def UpdatePosition(self):
+		threading.Timer(5.0, self.UpdatePosition).start()
+		if len(self.queue) > 0:
+			self.location = self.queue.pop()
+			print("Now on floor ", self.location)
