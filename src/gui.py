c9469a4a5b7f070e27f210b7238eb20d75c0d4f6
Spiky <ryan.j@epoxstudios.com>
2022-01-19 16:06:54 -0500

    Switched to pysimplegui, created initial window

--- src/gui.py
+++ src/gui.py
@@ -1,15 +1,9 @@
-import tkinter as tk
-from tkinter import *
+import PySimpleGUI as sg
 class GUI:
     def __init__(self, model):
         self.model = model
         model.PassGUI(self)
     def CreateWindow(self):
-        window = tk.Tk()
-        window.configure(background='white')
-        window.geometry("400x200")
-        button = Button(window, text = 'Submit', bg='#ffffff', activebackground='#00ff00')  
-        button.pack() 
-        window.mainloop()
+        sg.Window(title="Elevator Simulator", layout=[[]], margins=(400, 400)).read()
     def UpdateElevatorLocation(self, location):
         print("Change label to ", location)
