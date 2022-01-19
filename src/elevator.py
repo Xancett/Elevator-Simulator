1ea9ad3876eec5d0bf00e83480c5323e0865b060
Spiky <ryan.j@epoxstudios.com>
2022-01-19 09:49:39 -0500

    Fixed queue and added code for choosing a floor inside the elevator

--- src/elevator.py
+++ src/elevator.py
@@ -5,8 +5,8 @@ class Elevator:
 	def __init__(self, floors):
 		self.floors = floors
 		self.location = 0
-		self.up_queue = [floors]
-		self.down_queue = [floors]
+		self.up_queue = []
+		self.down_queue = []
 		self.direction = 0
 		self.UpdatePosition()
 	def getLocation(self):
@@ -20,22 +20,30 @@ class Elevator:
 			self.direction = 1 if up else -1
 	def ChooseFloor(self, floor):
 		print("Picked a floor")
+		if (floor > self.location):
+			self.up_queue.append(floor)
+		if (floor < self.location):
+			self.down_queue.append(floor)
 	def UpdatePosition(self):
 		threading.Timer(5.0, self.UpdatePosition).start()
 		self.CheckDirection()
 		if (self.direction == 1):
 			if (len(self.up_queue) > 0):
-				self.up_queue.sort()
+				self.up_queue.sort(reverse=True)
 				self.location = self.up_queue.pop()
 				print("Now on floor ", self.location)
+				print("Direction is up")
 		if (self.direction == -1):
 			if (len(self.down_queue) > 0):
-				self.down_queue.sort(reverse=True)
+				self.down_queue.sort()
 				self.location = self.down_queue.pop()
 				print("Now on floor ", self.location)
+				print("Direction is down")
 	def CheckDirection(self):
 		checkqueue = self.up_queue if self.direction == 1 else self.down_queue
 		for floor in checkqueue:
 			if math.copysign(1, floor - self.location) == self.direction:
 				return
 		self.direction *= -1
+		if len(self.up_queue) == 0 and len(self.down_queue) == 0:
+			self.direction = 0
