52b1248056d3399f0aae7097076f3d67810db40a
Spiky <ryan.j@epoxstudios.com>
2022-01-19 11:10:27 -0500

    Changed class to represent the view, added link to model

--- src/gui.py
+++ src/gui.py
@@ -1,10 +1,15 @@
 import tkinter as tk
 from tkinter import *
-window = tk.Tk()
-window.configure(background='white')
-window.geometry("400x200")
-button = Button(window, text = 'Submit', bg='#ffffff', activebackground='#00ff00')  
-button.pack()  
-
-
-window.mainloop()
+class GUI:
+    def __init__(self, model):
+        self.model = model
+        model.PassGUI(self)
+    def CreateWindow(self):
+        window = tk.Tk()
+        window.configure(background='white')
+        window.geometry("400x200")
+        button = Button(window, text = 'Submit', bg='#ffffff', activebackground='#00ff00')  
+        button.pack() 
+        window.mainloop()
+    def UpdateElevatorLocation(self, location):
+        print("Change label to ", location)
